965fc0086b5087c73ed7f100d8b517ab3537e990
olofuchs <oliver@farm-ng.com>
2024-03-15 20:25:33 -0700

    fix plotting bug

--- filter.py
+++ filter.py
@@ -7,5 +7,5 @@ databases = {
 
 with MDF("logs/00000001.MF4") as mdf_file:
     extracted = mdf_file.extract_bus_logging(database_files=databases)
-    cut = extracted.cut(start = 20, stop = 60, whence=1)
+    cut = extracted.cut(start = 10, stop = 25, whence=1)
     cut.export('csv',"csv/extracted.csv", add_units = True,)
--- plot.py
+++ plot.py
@@ -5,11 +5,13 @@ import pandas as pd
 TPDO1_A = "csv/extracted.ChannelGroup_0_CAN1_-_message_MOTOR_A_TPDO1_0x18A_EXT=False.csv"
 RPDO1_A = "csv/extracted.ChannelGroup_4_CAN1_-_message_MOTOR_A_RPDO1_0x20A_EXT=False.csv"
 
-tpd01_A = pd.read_csv(TPDO1_A)
-rpd01_A = pd.read_csv(RPDO1_A)
+tpd01_A = pd.read_csv(TPDO1_A,skiprows=[1])
+rpd01_A = pd.read_csv(RPDO1_A,skiprows=[1])
 
-# plt.plot(tpd01_A["timestamps"],tpd01_A["CAN1.MOTOR_A_TPDO1.RPM_A"],label = "tpd01",marker = "o")
-plt.plot(rpd01_A["timestamps"],rpd01_A["CAN1.MOTOR_A_RPDO1.REM_SPEED_A"],label = "rpd01",marker = "x")
+plt.plot(tpd01_A["timestamps"],tpd01_A["CAN1.MOTOR_A_TPDO1.RPM_A"],label = "tpd01_rpm_A",marker = "o")
+plt.plot(rpd01_A["timestamps"],rpd01_A["CAN1.MOTOR_A_RPDO1.REM_SPEED_A"],label = "rpd01_rpm_A",marker = "x")
+
 plt.legend()
 plt.show()
-print(rpd01_A["timestamps"])
+
+
